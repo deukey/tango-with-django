aee3b95a5289e6a7934327cbe2f4c4404dd31870
deukey <deukey@naver.com>
2015-08-28 20:23:40 +0900

    debug on

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from .models import Category
 
 
 def index(request):
-    return render(request, 'rango/index.html', {'boldmessage': 'I am bold font.'})
-
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
     return render(request, 'rango/about.html', {})
