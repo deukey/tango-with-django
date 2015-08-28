2a74675bdc6f6c18f87dde941ca49e21b913307d
deukey <deukey@naver.com>
2015-08-28 11:05:53 +0900

    0.01 Added basic views and static files

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'rango/index.html', {'boldmessage': 'I am bold font.'})
+
+
+def about(request):
+    return HttpResponse('This is about page.<br/><a href="/rango">Index</a>')
