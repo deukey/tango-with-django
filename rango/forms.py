e4b20c4d2cec06e404cbb05d0648a67512e358ca
deukey <deukey@naver.com>
2015-09-01 10:51:18 +0900

    added add_category

--- rango/forms.py
+++ rango/forms.py
@@ -10,6 +10,22 @@ class CategoryForm(forms.ModelForm):
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
 
+    def is_valid(self):
+        valid = super(CategoryForm, self).is_valid()
+
+        if not valid:
+            return valid
+
+        else:
+            try:
+                self.cleaned_data['name'].decode('ascii')
+            except UnicodeDecodeError:
+                self._errors['non_ascii'] = '카테고리 이름은 영문이나 숫자, 하이픈만 가능합니다.'
+                # raise forms.ValidationError('bad value')
+                return False
+
+        return True
+
     class Meta:
         model = Category
         fields = ('name',)
@@ -17,3 +33,9 @@ class CategoryForm(forms.ModelForm):
 
 class PageForm(forms.ModelForm):
     title = forms.CharField(max_length=128, help_text="페이지의 제목을 넣으세요.")
+    url = forms.URLField(max_length=200, help_text="페이지의 URL을 넣으세요.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+    class Meta:
+        model = Page
+        exclude = ('category',)
--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Page
+from .forms import CategoryForm
 
 
 def index(request):
@@ -16,3 +17,19 @@ def about(request):
 def category(request, category_name_slug):
     category = get_object_or_404(Category, slug=category_name_slug)
     return render(request, 'rango/category.html', {'category': category})
+
+
+def add_category(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return index(request)
+        else:
+            print form.errors
+
+    else:
+        form = CategoryForm()
+
+    return render(request, 'rango/add_category.html', {'form': form})
