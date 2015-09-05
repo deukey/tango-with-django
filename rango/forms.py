181310a37bf3c04b868f6dfab0d7471c1ec5daf6
deukey <deukey@naver.com>
2015-09-05 10:03:13 +0900

    clean() override

--- rango/forms.py
+++ rango/forms.py
@@ -16,15 +16,13 @@ class CategoryForm(forms.ModelForm):
         if not valid:
             return valid
 
-        else:
-            try:
-                self.cleaned_data['name'].decode('ascii')
-            except UnicodeDecodeError:
-                self._errors['non_ascii'] = '카테고리 이름은 영문이나 숫자, 하이픈만 가능합니다.'
-                # raise forms.ValidationError('bad value')
-                return False
+        elif any(c.isalnum() for c in self.cleaned_data['name']):
+            # 영문이나 숫자가 하나라도 포함되어 있어야 슬러그가 제대로 생성될 수 있으므로 이를 체크
+            return True
 
-        return True
+        else:
+            self._errors['name'] = '영문이나 숫자가 하나라도 포함되어야 합니다.'
+            return False
 
     class Meta:
         model = Category
@@ -35,6 +33,16 @@ class PageForm(forms.ModelForm):
     title = forms.CharField(max_length=128, help_text="페이지의 제목을 넣으세요.")
     url = forms.URLField(max_length=200, help_text="페이지의 URL을 넣으세요.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        if url and not url.startswith('http://'):
+            url = 'http://' + url
+            cleaned_data['url'] = url
+
+        return cleaned_data
 
     class Meta:
         model = Page
