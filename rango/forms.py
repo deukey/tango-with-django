0bbbd10d91fd032bee4b5ac7f53984c1cd677bf6
deukey <deukey@naver.com>
2015-09-05 13:55:35 +0900

    added add_page

--- rango/forms.py
+++ rango/forms.py
@@ -2,27 +2,37 @@
 
 from django import forms
 from .models import Category, Page
+from django.core.exceptions import ValidationError
+
+
+def validate_can_make_slug(value):
+    if not any(c.isalnum() for c in value):
+            # 영문이나 숫자가 하나라도 포함되어 있어야 슬러그가 제대로 생성될 수 있으므로 이를 체크
+        raise ValidationError('영문이나 숫자가 하나라도 포함되어야 합니다.')
 
 
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=128, help_text="카테고리 이름을 넣으세요(영문, 숫자만 가능).")
+    korean_error_messages = {'required': '카테고리 이름을 꼭 넣어주세요.', 'unique': '동일한 이름의 카테고리가 이미 존재합니다.'}
+    name = forms.CharField(max_length=128, help_text="카테고리 이름을 넣으세요(영문, 숫자만 가능).",
+                           validators=[validate_can_make_slug],
+                           error_messages=korean_error_messages)
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
 
-    def is_valid(self):
-        valid = super(CategoryForm, self).is_valid()
-
-        if not valid:
-            return valid
-
-        elif any(c.isalnum() for c in self.cleaned_data['name']):
-            # 영문이나 숫자가 하나라도 포함되어 있어야 슬러그가 제대로 생성될 수 있으므로 이를 체크
-            return True
-
-        else:
-            self._errors['name'] = '영문이나 숫자가 하나라도 포함되어야 합니다.'
-            return False
+    # def is_valid(self):
+    #     valid = super(CategoryForm, self).is_valid()
+    #
+    #     if not valid:
+    #         return valid
+    #
+    #     elif any(c.isalnum() for c in self.cleaned_data['name']):
+    #         # 영문이나 숫자가 하나라도 포함되어 있어야 슬러그가 제대로 생성될 수 있으므로 이를 체크
+    #         return True
+    #
+    #     else:
+    #         self._errors['name'] = '영문이나 숫자가 하나라도 포함되어야 합니다.'
+    #         return False
 
     class Meta:
         model = Category
--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,8 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Page
-from .forms import CategoryForm
+from .forms import CategoryForm, PageForm
 
 
 def index(request):
@@ -15,8 +17,9 @@ def about(request):
 
 
 def category(request, category_name_slug):
-    category = get_object_or_404(Category, slug=category_name_slug)
-    return render(request, 'rango/category.html', {'category': category})
+    cat = get_object_or_404(Category, slug=category_name_slug)
+    context_dict = {'category': cat, 'cat_slug': category_name_slug}
+    return render(request, 'rango/category.html', context_dict)
 
 
 def add_category(request):
@@ -25,7 +28,7 @@ def add_category(request):
 
         if form.is_valid():
             form.save()
-            return index(request)
+            return index(request) # 리다이렉트로 바꿔
         else:
             print form.errors
 
@@ -33,3 +36,28 @@ def add_category(request):
         form = CategoryForm()
 
     return render(request, 'rango/add_category.html', {'form': form})
+
+
+def add_page(request, category_name_slug):
+    try:
+        cat = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        cat = None
+
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if cat:
+                page = form.save(commit=False)
+                page.category = cat
+                page.views = 0
+                page.save()
+                return category(request, category_name_slug) # 리다이렉트로 바꿔
+        else:
+            print form.errors
+    else:
+        form = PageForm()
+
+    context_dict = {'form': form, 'category': cat}
+
+    return render(request, 'rango/add_page.html', context_dict)
