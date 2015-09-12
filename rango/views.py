806b08a3c860a7adb1366e37eb17b199fbdc404c
deukey <deukey@naver.com>
2015-09-12 21:29:01 +0900

    Ch11. Cookie Applied.

--- rango/views.py
+++ rango/views.py
@@ -6,13 +6,39 @@ from .models import Category, Page
 from .forms import CategoryForm, PageForm, PageEditForm, UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'top_categories': category_list, 'top_pages': page_list}
-    return render(request, 'rango/index.html', context_dict)
+
+    visits = int(request.COOKIES.get('visits', '1'))
+
+    reset_last_visit_time = False
+
+    if 'last_visit' in request.COOKIES:
+        last_visit = request.COOKIES['last_visit']
+        last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+
+        if (datetime.now() - last_visit_time).seconds > 5:
+            visits += 1
+            reset_last_visit_time = True
+
+        context_dict['visits'] = visits
+        response = render(request, 'rango/index.html', context_dict)
+
+    else:
+        reset_last_visit_time = True
+        context_dict['visits'] = visits
+        response = render(request, 'rango/index.html', context_dict)
+
+    if reset_last_visit_time:
+        response.set_cookie('last_visit', datetime.now())
+        response.set_cookie('visits', visits)
+
+    return response
 
 
 def about(request):
